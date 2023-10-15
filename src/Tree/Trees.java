28c078892927f83feb0027e2b287f6e0f2766fb0
dev-rohitsaini <rohitsaini1337@gmail.com>
2023-10-15 13:49:51 +0530

    add opreation done

---
+++ src/Tree/Trees.java
@@ -0,0 +1,34 @@
+package Tree;
+
+public class Trees {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class BinaryTreeX{
+        static int idx = -1;
+        public static Node buildTree(int[] nodes){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+    public static void main(String[]  args){
+        int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTreeX tree = new BinaryTreeX();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+
+    }
+}
