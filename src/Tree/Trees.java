ebbe2c382a5674092ab6bf7aa157d1beefa914dd
dev-rohitsaini <rohitsaini1337@gmail.com>
2023-12-10 16:33:02 +0530

    modification in tree

--- src/Tree/Trees.java
+++ src/Tree/Trees.java
@@ -1,34 +1,30 @@
 package Tree;
 
 public class Trees {
-    static class Node {
-        int data;
-        Node left;
-        Node right;
-        Node(int data){
-            this.data = data;
-            this.left = null;
-            this.right = null;
+        class Node{
+            int data;
+            Node left;
+            Node right;
+            Node(int data){
+                this.data = data;
+                this.left = null;
+                this.right = null;
+            }
         }
-    }
-    static class BinaryTreeX{
-        static int idx = -1;
-        public static Node buildTree(int[] nodes){
+        static int idx=-1;
+        public Node buildTree(int[] values){
             idx++;
-            if(nodes[idx]==-1){
+            if(idx == -1){
                 return null;
             }
-            Node newNode = new Node(nodes[idx]);
-            newNode.left = buildTree(nodes);
-            newNode.right = buildTree(nodes);
+            Node newNode = new Node(values[idx]);
+            newNode.left = buildTree(values);
+            newNode.right = buildTree(values);
             return newNode;
         }
-    }
     public static void main(String[]  args){
-        int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        BinaryTreeX tree = new BinaryTreeX();
-        Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+            int[] nodeValues = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+            
 
     }
 }
