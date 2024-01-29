a0311ca9d6ec605a5b8344212e0d838c8bc82775
dev-rohitsaini <rohitsaini1337@gmail.com>
2024-01-29 23:57:53 +0530

    first program Completed successfully

--- src/ArrayDemoData/DummyArrays.java
+++ src/ArrayDemoData/DummyArrays.java
@@ -12,4 +12,18 @@ public class DummyArrays {
         }
         return arr;
     }
+    public int[] staticArray(){
+        int[] arr = {1,4,7,3,3,4,5,79,5,2,2,4,6,7,9};
+        return arr;
+    }
+    public int[] ststicSortedArray(){
+        int[] arr = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+        return arr;
+    }
+    public void prinArray(int[] arr){
+        int size = arr.length;
+        for(int i = 0; i < size; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
--- src/DsaEasyQuestions/MinMaxInArray.java
+++ src/DsaEasyQuestions/MinMaxInArray.java
@@ -1,7 +1,33 @@
 package DsaEasyQuestions;
+import ArrayDemoData.DummyArrays;
 
 public class MinMaxInArray {
     public static void main(String args[]){
+        DummyArrays obj = new DummyArrays();
+        MinMaxInArray obj2 = new MinMaxInArray();
+        int arr[] = obj.staticArray();
+        int[] res = obj2.minAndMaxValue(arr);
+        System.out.print("The maximum value is "+res[1]+" and minimum value in array is "+res[0]);
 
     }
+    int[] minAndMaxValue(int[] nums){
+        int[] minMaxValue = {nums[0], nums[0]};
+        int size = nums.length;
+        for (int num : nums) {
+            if (minMaxValue[0] > num) {
+                minMaxValue[0] = num;
+            }
+            if (minMaxValue[1] < num) {
+                minMaxValue[1] = num;
+            }
+        }
+        return minMaxValue;
+    }
+     int[] swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        return nums;
+     }
+
 }
